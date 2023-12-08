3ebac6a8815382048a23a2be1eb2f22f0a365dfb
Hasan-Ali-murrar <hasanalimurrar@gmail.com>
2023-12-08 23:43:13 +0200

    add  new  folder

--- project-hasan/routes/web.php
+++ project-hasan/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('search');
+    return view('pages/search');
 });
 
 Route::get('/dashboard', function () {
