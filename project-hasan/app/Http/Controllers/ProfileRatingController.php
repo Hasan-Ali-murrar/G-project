39a486ba87e0446ef9da87acdcbf09dd78031997
Hasan-Ali-murrar <hasanalimurrar@gmail.com>
2023-12-11 22:42:14 +0200

    Create tables and add fields

---
+++ project-hasan/app/Http/Controllers/ProfileRatingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile_Rating;
+use Illuminate\Http\Request;
+
+class ProfileRatingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Profile_Rating $profile_Rating)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Profile_Rating $profile_Rating)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Profile_Rating $profile_Rating)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Profile_Rating $profile_Rating)
+    {
+        //
+    }
+}
---
+++ project-hasan/app/Http/Controllers/ProfileRatingsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile_Ratings;
+use Illuminate\Http\Request;
+
+class ProfileRatingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Profile_Ratings $profile_Ratings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Profile_Ratings $profile_Ratings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Profile_Ratings $profile_Ratings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Profile_Ratings $profile_Ratings)
+    {
+        //
+    }
+}
